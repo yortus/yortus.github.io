a40441823c972450b5c6e325cab016bacf6e07aa
yortus <yortus@gmail.com>
2015-06-21 18:03:06 +0800

    integrated ko and ts

--- games/caveman/game.ts
+++ games/caveman/game.ts
@@ -1,13 +1,19 @@
-///<reference path="./typings/jquery/jquery.d.ts" />
+﻿///<reference path="./typings/jquery/jquery.d.ts" />
 ///<reference path="./typings/knockout/knockout.d.ts" />
 ///<reference path="./main.ts" />
+declare var kd;
+
+
 // Set initial game state.
 var levels = []; // : { map: { [row: number]: { [col: number]: string; }; }, width: number; height: number; }
 var currentLevel = 0;
 var lives = 3;
 var keys = 0;
 var jewels = 0;
+
 var cave = null;
+
+
 var legend = {
     ' ': 'blank',
     '#': 'rocks',
@@ -15,33 +21,44 @@ var legend = {
     'D': 'door-gold',
     'V': 'diamond'
 };
-var gameVM;
+
+
+var gameVM: game.GameVM;
+
+
+
 // Start the game when the page has loaded.
 $(document).ready(function () {
-    var rawLevels = getRawLevels();
+
+    var rawLevels = getRawLevels();    
     gameVM = new game.GameVM(rawLevels);
     ko.applyBindings(gameVM);
     $('#cave-outer')
         .scrollTop(gameVM.levelMap.playerStartY() * 32 - 320)
         .scrollLeft(gameVM.levelMap.playerStartX() * 32 - 320);
-    //$('#player')
-    //    .css({ 'margin-left': gameVM.levelMap.playerStartX() * 32, 'margin-top': gameVM.levelMap.playerStartY() * 32});
+    
     enableGamepad();
     startGameLoop();
-    //    $('#credits-show').click(function() { $('#credits-popup').show(); });
-    //    $('#credits-hide').click(function() { $('#credits-popup').hide(); });
+//    $('#credits-show').click(function() { $('#credits-popup').show(); });
+//    $('#credits-hide').click(function() { $('#credits-popup').hide(); });
 });
+
+
 function getRawLevels() {
     var rawLevelMaps = $('#level-maps').text();
     var rawLevels = rawLevelMaps.split(/L[0-9]+[ ]*\r?\n/).slice(1);
     return rawLevels;
 }
+
+
 var gamepad = {
     UP: { isDown: false },
     DOWN: { isDown: false },
     LEFT: { isDown: false },
     RIGHT: { isDown: false }
-};
+}
+
+
 function enableGamepad() {
     $('#gamepad .up').on('mousedown touchstart', function (e) { gamepad.UP.isDown = true; e.preventDefault(); });
     $('#gamepad .up').on('mouseup mouseout touchend', function (e) { gamepad.UP.isDown = false; e.preventDefault(); });
@@ -52,18 +69,20 @@ function enableGamepad() {
     $('#gamepad .right').on('mousedown touchstart', function (e) { gamepad.RIGHT.isDown = true; e.preventDefault(); });
     $('#gamepad .right').on('mouseup mouseout touchend', function (e) { gamepad.RIGHT.isDown = false; e.preventDefault(); });
 }
+
+
 function startGameLoop() {
     setTimeout(update, 100);
 }
+
+
 function isRocksAt(x, y) {
     var p = gameVM.levelMap.cells()[y]()[x];
     return p === '#';
 }
 function isBlockedAt(x, y) {
-    if (x < 0 || y < 0)
-        return true;
-    if (x >= gameVM.levelMap.width() || y >= gameVM.levelMap.height())
-        return true;
+    if (x < 0 || y < 0) return true;
+    if (x >= gameVM.levelMap.width() || y >= gameVM.levelMap.height()) return true;
     var p = gameVM.levelMap.cells()[y]()[x];
     return p === '#' || p === 'D';
 }
@@ -79,9 +98,12 @@ function isDiamondAt(x, y) {
     var p = gameVM.levelMap.cells()[y]()[x];
     return p === 'V';
 }
+
+
 function update() {
     var level = levels[currentLevel];
     var player = gameVM.player;
+
     // Move/draw player
     var UP = kd.UP.isDown() || gamepad.UP.isDown;
     var DOWN = kd.DOWN.isDown() || gamepad.DOWN.isDown;
@@ -89,6 +111,7 @@ function update() {
     var RIGHT = kd.RIGHT.isDown() || gamepad.RIGHT.isDown;
     var dx = LEFT ? -1 : RIGHT ? 1 : 0;
     var dy = UP ? -1 : DOWN ? 1 : 0;
+
     // Hit door?
     if (isDoorAt(player.x() + dx, player.y() + dy)) {
         if (gameVM.keysCollected() > 0) {
@@ -96,15 +119,13 @@ function update() {
             gameVM.levelMap.cells()[player.y() + dy].splice(player.x() + dx, 1, ' ');
         }
     }
+ 
     // Hit wall, door, etc?
     if (isBlockedAt(player.x() + dx, player.y() + dy)) {
         if (dx != 0 && dy != 0) {
-            if (!isBlockedAt(player.x() + dx, player.y()))
-                dy = 0;
-            else if (!isBlockedAt(player.x(), player.y() + dy))
-                dx = 0;
-            else
-                dx = dy = 0;
+            if (!isBlockedAt(player.x() + dx, player.y()))        dy = 0;
+            else if (!isBlockedAt(player.x(), player.y() + dy))   dx = 0;
+            else                                                dx = dy = 0;
         }
         else {
             dx = dy = 0;
@@ -118,23 +139,28 @@ function update() {
         $('#cave-outer').scrollTop($('#cave-outer').scrollTop() + dy * 32);
     }
     dx = dy = 0;
-    // Hit key?
-    if (isKeyAt(player.x(), player.y())) {
+
+     // Hit key?
+     if (isKeyAt(player.x(), player.y())) {
         gameVM.keysCollected(gameVM.keysCollected() + 1);
         gameVM.levelMap.cells()[player.y()].splice(player.x(), 1, ' ');
-    }
+     }    
+ 
     // Hit diamond?
     if (isDiamondAt(player.x(), player.y())) {
         gameVM.jewelsCollected(gameVM.jewelsCollected() + 1);
         gameVM.levelMap.cells()[player.y()].splice(player.x(), 1, ' ');
     }
+
     //// Fell in a hole?
     //if (isHoleAt(player.x, player.y)) {
+
     //    // Player died!
     //    --lives;
     //    $('#cave').addClass('died');
     //    setTimeout(function () {
     //        if (lives <= 0) {
+
     //            // Player loses game!
     //            alert('Oh no! You lose the game!!! Better luck next time.');
     //            lives = 3;
@@ -149,14 +175,18 @@ function update() {
     //        startGameLoop();
     //    }, 1000);
     //}
+
     //// Found treasure?
     //else if (isTreasureAt(player.x, player.y)) {
+
     //    // Player wins level!
     //    $('#player').addClass('wins');
     //    $('#cave').addClass('wins');
+
     //    setTimeout(function() {
     //        ++currentLevel;
     //        if (currentLevel >= levels.length) {
+
     //            // Player wins game!
     //            alert('YOU\'RE THE CAVE MAN CHAMPION!!!');
     //            currentLevel = 0;
@@ -165,9 +195,11 @@ function update() {
     //        loadCurrentLevel();
     //        startGameLoop();
     //    }, 2000);
+
     //}
+
     // Nothing happened, keep playing....
     //else {
-    setTimeout(update, 100);
+        setTimeout(update, 100);
     //}
 }
